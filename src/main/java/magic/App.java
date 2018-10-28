37e541d16bd6f0ad76b4fdcf89a4cda9dbe31fd2
Pavel <Pavel@169.254.109.173>
2018-10-28 19:27:04 +0200

    0.1.0 version

--- src/main/java/magic/App.java
+++ src/main/java/magic/App.java
@@ -8,6 +8,6 @@ import magic.container.TypesContainerImpl;
 public class App {
     public static void main(String[] args) throws IOException {
        MimeResolver r = new MimeResolver(TypesContainerImpl.defaultContainer());
-       System.out.println(r.confirmMimeType(new File("test.7z"), "application/x-7z-compressed"));
+       System.out.println(r.confirmMimeType(new File(args[0]), args[1]));
     }
 }
